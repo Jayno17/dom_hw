f9dc0b07cb70f1cd70b81a61b284f12bc6ecdfda
Jayno17 <julia.nowak@gmail.com>
2020-02-21 17:54:33 +0000

    added event listeners to app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const newItemForm = document.querySelector('#new-item-form');
+  newItemForm.addEventListener('submit', handleFormSubmit);
 
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllClick);
 
+})
+
+const handleFormSubmit = function (event){
+  event.preventDefault();
+
+  const trackListItem = createTrackListItem(event.target);
+  const trackList = document.querySelector('#track-list');
+  trackList.appendChild(trackListItem);
+
+  event.target.reset();
 }
