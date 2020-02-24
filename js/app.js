e58fd4b8db420dad8803cb2f2dac3173a641bd65
Jayno17 <julia.nowak@gmail.com>
2020-02-24 07:31:31 +0000

    added form elements

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const newItemForm = document.querySelector('#new-item-form');
-  newItemForm.addEventListener('submit', handleFormSubmit);
+  const formItem = document.querySelector('#form-item');
+  formItem.addEventListener('submit', handleFormItemSubmit);
 
   const deleteAllButton = document.querySelector('#delete-all');
   deleteAllButton.addEventListener('click', handleDeleteAllClick);
 
-})
+});
 
-const handleFormSubmit = function (event){
+const handleFormItemSubmit = function(event) {
   event.preventDefault();
 
   const trackListItem = createTrackListItem(event.target);
@@ -15,4 +15,23 @@ const handleFormSubmit = function (event){
   trackList.appendChild(trackListItem);
 
   event.target.reset();
+}
+
+ const createTrackListItem = function (form) {
+  const trackListItem = document.createElement('li');
+  trackListItem.classList.add('track-list-item');
+
+  const title = document.createElement('h2');
+  title.textContent = form.title.value;
+  trackListItem.appendChild(title);
+
+  const artist = document.createElement('h3');
+  artist.textContent = form.artist.value;
+  trackListItem.appendChild(artist);
+
+  const genre = document.createElement('h3');
+  genre.textContent = form.genre.value;
+  trackListItem.appendChild(genre);
+
+  return trackListItem;
 }
